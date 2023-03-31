1e48171d609920ab7d4b466970da71ab3117b90a
volinad86git config --global user.name volinad86 <dima_student2005@mail.ru>
2023-03-31 10:43:03 -0400

    adding variables

---
+++ script.js
@@ -0,0 +1,10 @@
+// assigning variables to the forward and backward buttons
+const back = document.querySelector('.back');
+const next = document.querySelector('.next');
+// set a variable for photos and add them
+const photos = ['image/1NY.jpg',  'image/2NY.jpg', 'image/3NY.jpg', 'image/4NY.jpg', 'image/5NY.jpg','image/6NY.jpg'];
+console.log(photos [4])
+//set the variable i where it is the index.
+let i = 0;
+
+
